c86420473b5e075ce85da793b86f167a15017d1f
Victor Meireles <victoraamlima@gmail.com>
2024-01-12 22:08:54 -0300

    Add Home component and adjust gap in Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import Header from "../Header/Header"
+import { Home } from "../Home/Home"
 import { StyledMain } from "./styleMain"
 
 const Main = () => {
   return (
     <StyledMain>
       <Header />
+      <Home />
     </StyledMain>
   )
 }
--- src/components/Main/styleMain.js
+++ src/components/Main/styleMain.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 const StyledMain = styled.main`
   display: flex;
   flex-direction: column;
+  gap: 100px;
   background-color: #ebe;
 `
 
