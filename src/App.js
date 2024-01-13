f74ad1dcfaf078a6b80f39560f95203e3e2b02b1
Victor Meireles <victoraamlima@gmail.com>
2024-01-12 22:07:31 -0300

    Add Redux store provider to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { Provider } from "react-redux"
+import store from "./components/store/store"
 import Main from "./components/Main/Main"
 import { GlobalStyle } from "./style/globalStyle"
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <Main />
+      <Provider store={store}>
+        <GlobalStyle />
+        <Main />
+      </Provider>
     </>
-    
   )
 }
 
