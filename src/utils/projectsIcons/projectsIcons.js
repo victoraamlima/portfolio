fce29d265ff678366718b8bd77ec62ba7b821dd7
Victor Meireles <victoraamlima@gmail.com>
2024-01-15 22:15:19 -0300

    Add project icons for various technologies

---
+++ src/utils/projectsIcons/projectsIcons.js
@@ -0,0 +1,29 @@
+import ReduxSvg from "../../images/technologies/redux.svg"
+import CssSvg from "../../images/technologies/css.svg"
+import FigmaSvg from "../../images/technologies/figma.svg"
+import GitSvg from "../../images/technologies/git.svg"
+import HtmlSvg from "../../images/technologies/html.svg"
+import JsSvg from "../../images/technologies/js.svg"
+import JestSvg from "../../images/technologies/jest.svg"
+import JquerySvg from "../../images/technologies/jquery.svg"
+import NodejsSvg from "../../images/technologies/nodejs.svg"
+import NpmSvg from "../../images/technologies/npm.svg"
+import ReactSvg from "../../images/technologies/react.svg"
+import PhotoshopSvg from "../../images/technologies/photoshop.svg"
+
+const technologiesSvgs = [
+  { id: "redux", name: "Redux", svg: ReduxSvg },
+  { id: "css", name: "CSS", svg: CssSvg },
+  { id: "figma", name: "Figma", svg: FigmaSvg },
+  { id: "git", name: "Git", svg: GitSvg },
+  { id: "html", name: "HTML", svg: HtmlSvg },
+  { id: "js", name: "JavaScript", svg: JsSvg },
+  { id: "jest", name: "Jest", svg: JestSvg },
+  { id: "jquery", name: "jQuery", svg: JquerySvg },
+  { id: "nodejs", name: "Node.js", svg: NodejsSvg },
+  { id: "npm", name: "NPM", svg: NpmSvg },
+  { id: "react", name: "React", svg: ReactSvg },
+  { id: "photoshop", name: "Photoshop", svg: PhotoshopSvg },
+]
+
+export default technologiesSvgs
