6c9a901ef963066f84d4c73f2f4a876800c74d44
Victor Meireles <victoraamlima@gmail.com>
2024-01-17 11:56:19 -0300

    Refactor Home component and styleHome.js
    This commit refactors the Home component and styleHome.js file. It removes the ContactBtn import, updates the ProfileInfo component, and removes unnecessary styles.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,11 @@
-import { ContactBtn } from "../ContactBtn/ContactBtn"
 import perfioFoto from "../../images/profile.jpg"
-import { ProfileFoto, ProfileInfo, StyledHome } from "./styleHome"
+import { ProfileFoto, StyledHome } from "./styleHome"
+import ProfileInfo from "../ProfileInfo/ProfileInfo"
 
 const Home = () => {
   return (
     <StyledHome>
-      <ProfileInfo>
-        <div>
-          <h4>Oi, eu sou</h4>
-          <h2>Victor Meireles.</h2>
-          <h3>Desenvolvedor Front-End.</h3>
-        </div>
-
-        <p>
-          Desde que interessei-me pela área da programação, há um ano, tenho
-          estudado e praticado diariamente. Além de, ter feito diversos
-          projetos.
-        </p>
-
-        <ContactBtn />
-      </ProfileInfo>
+      <ProfileInfo footer={false} />
 
       <ProfileFoto src={perfioFoto} alt="Foto do perfio do Victor Meireles" />
     </StyledHome>
--- src/components/Home/styleHome.js
+++ src/components/Home/styleHome.js
@@ -6,55 +6,6 @@ const StyledHome = styled.section`
   padding: 0 100px;
   gap: 100px;
 `
-const ProfileInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 40px;
-  max-width: 900px;
-
-  div {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-  }
-
-  h2 {
-    color: ${({ theme }) =>
-      theme.theme === "light" ? theme.colors.blue3 : theme.colors.white1};
-    font-family: Inter;
-    font-size: 80px;
-    font-weight: 700;
-    line-height: 100%;
-  }
-
-  h3 {
-    color: ${({ theme }) =>
-      theme.theme === "light" ? theme.colors.blue3 : theme.colors.white1};
-    font-family: Inter;
-    font-size: 40px;
-    font-weight: 600;
-    line-height: 100%;
-  }
-
-  h4 {
-    color: ${({ theme }) =>
-      theme.theme === "light" ? theme.colors.blue3 : theme.colors.white1};
-    font-family: Inter;
-    font-size: 48px;
-    font-weight: 500;
-    line-height: 100%;
-  }
-
-  p {
-    color: ${({ theme }) =>
-      theme.theme === "light" ? theme.colors.blue1 : theme.colors.white1};
-    font-family: Inter;
-    font-size: 32px;
-    font-weight: 400;
-    margin-bottom: 10px;
-  }
-`
 
 const ProfileFoto = styled.img`
   width: 500px;
@@ -63,4 +14,4 @@ const ProfileFoto = styled.img`
   object-fit: cover;
 `
 
-export { StyledHome, ProfileInfo, ProfileFoto }
+export { StyledHome, ProfileFoto }
